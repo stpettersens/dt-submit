655bd7e85e7e1f12d735efea08c7ae0539e3adc6
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-07-25 14:33:54 +0100

    ...

---
+++ typings/github/github.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for github
+// Project: https://github.com/mikedeboer/node-github
+// Definitions by: Sam Saint-Pettersen <https://github.com/stpettersens>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+declare module "github" {
+	export class GitHubApi {
+		constructor(options: Object);
+	}
+}
